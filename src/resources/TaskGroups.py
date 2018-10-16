944047b493b27382094c524c80d73dc660dd93c6
Matt Hough <matthough1@icloud.com>
2018-10-15 22:06:12 -0400

    added remaining api methods

--- src/resources/TaskGroups.py
+++ src/resources/TaskGroups.py
@@ -40,3 +40,37 @@ class TaskGroupResource(Resource):
         result = taskgroup_schema.dump(taskGroup).data
 
         return {"status": 'success', 'data': result}, 201
+
+    def put(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = taskgroup_schema.load(json_data)
+        if errors:
+            return errors, 422
+        taskGroup = TaskGroup.query.filter_by(id=data['id']).first()
+        if not taskGroup:
+            return {'message': 'Task Group does not exist'}, 400
+        taskGroup.name = data['name']
+        taskGroup.description = data['description']
+        db.session.commit()
+
+        result = taskgroup_schema.dump(taskGroup).data
+
+        return {"status": 'success', 'data': result}, 204
+
+    def delete(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = taskgroup_schema.load(json_data)
+        if errors:
+            return errors, 422
+        taskGroup = TaskGroup.query.filter_by(id=data['id']).delete()
+        db.session.commit()
+
+        result = taskgroup_schema.dump(taskGroup).data
+
+        return {"status": 'success', 'data': result}, 204
