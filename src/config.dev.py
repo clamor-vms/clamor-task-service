6da974b986e663226ecd0588eac0c2752d4c903d
Matt Hough <matthough1@icloud.com>
2018-10-15 22:35:20 -0400

    updated config to be valid docker image / setting

--- src/config.dev.py
+++ src/config.dev.py
@@ -1,10 +1,11 @@
 import os
 
 # docker example:
-# docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:tag
+# docker run --name clamour -e MYSQL_ROOT_PASSWORD=your-secret-pw
+# -e MYSQL_DATABASE=task -p 3306:3306 -d mysql:5.7
 # You need to replace the next values with
 # the appropriate values for your configuration
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql://username:password@localhost/database_name"
+SQLALCHEMY_DATABASE_URI = "mysql://username:password@127.0.0.1/database_name"
